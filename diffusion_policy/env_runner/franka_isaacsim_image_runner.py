f981bae4ae0faaada4b72a7d6ea6c7f12413128d
Xiaoshen Han <hxs001@sjtu.edu.cn>
2024-10-13 13:33:09 +0800

    config

---
+++ diffusion_policy/env_runner/franka_isaacsim_image_runner.py
@@ -0,0 +1,23 @@
+import wandb
+import numpy as np
+import torch
+import collections
+import pathlib
+import tqdm
+import dill
+import math
+import wandb.sdk.data_types.video as wv
+from diffusion_policy.env.pusht.pusht_image_env import PushTImageEnv
+from diffusion_policy.gym_util.async_vector_env import AsyncVectorEnv
+# from diffusion_policy.gym_util.sync_vector_env import SyncVectorEnv
+from diffusion_policy.gym_util.multistep_wrapper import MultiStepWrapper
+from diffusion_policy.gym_util.video_recording_wrapper import VideoRecordingWrapper, VideoRecorder
+
+from diffusion_policy.policy.base_image_policy import BaseImagePolicy
+from diffusion_policy.common.pytorch_util import dict_apply
+from diffusion_policy.env_runner.base_image_runner import BaseImageRunner
+
+class FrankaIsaacSimImageRunner(BaseImageRunner):
+    def run(self, policy: BaseImagePolicy):
+        pass
+        return {}
